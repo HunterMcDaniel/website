95011a44099f877bc0600ebb276b3d8c9c891940
“Hunter <hunterlmcdaniel@ufl.edu>
2020-06-23 22:38:30 -0400

    form works now

---
+++ form/default.inc.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+
+  <title>Hunter McDaniel Portfolio Site</title>
+
+  <link type="text/css" rel="stylesheet" href="style.css" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<header>
+<h2>Hunter McDaniel <br> Media</h2>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+  </nav>
+</header>
+
+<div class="container">
+  <h1>Sorry something went wrong</h1>
+  <p>Please try again</p>
+</div>
+
+<footer>
+  <p>Hunter McDaniel Media 2020</p>
+</footer>
---
+++ form/thankyou.inc.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <meta charset="utf-8">
+
+  <title>Hunter McDaniel Portfolio Site</title>
+
+  <link type="text/css" rel="stylesheet" href="style.css" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<header>
+<h2>Hunter McDaniel <br> Media</h2>
+  <nav>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="portfolio.html">Portfolio</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="contact.html">Contact</a></li>
+    </ul>
+  </nav>
+  </header>
+
+<main class="thank">
+
+<h1>Thank you for your enquiry</h1>
+<p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+
+</main>
+
+<footer>
+  <p>Hunter McDaniel Media 2020</p>
+</footer>
+</html>
